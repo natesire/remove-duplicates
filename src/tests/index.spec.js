c5eaf0f85c56663ea977121ac9563d84d91eff96
Nate Sire <nate.sire@fairwaymc.com>
2023-03-04 08:26:20 -0500

    pretty

--- src/tests/index.spec.js
+++ src/tests/index.spec.js
@@ -5,55 +5,49 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
 const Schema_js_1 = __importDefault(require("../Schema.js"));
-describe('schema', () => {
+describe('Schema', () => {
     let schemaFilenameWithPath;
     let schemaInstance;
+    let schemaContent;
+    let schemaObj = {
+        test: "test"
+    };
     beforeAll(() => {
         schemaFilenameWithPath = 'mock.json';
+        schemaContent = fs_1.default.readFileSync(schemaFilenameWithPath, 'utf8');
     });
-    describe('setup', () => {
-        it('should stringify schema as a string', () => {
-            let schemaObj = {
-                test: "test"
-            };
-            let str = JSON.stringify(schemaObj);
-            expect(str).toMatch('test');
-        });
+    it('should receive an object', () => {
+        let schemaObj = JSON.parse(schemaContent);
+        schemaInstance = new Schema_js_1.default(schemaObj);
+        expect(schemaInstance).toBeTruthy();
     });
-    describe('schema', () => {
-        let schemaContent;
-        beforeAll(() => {
-            schemaContent = fs_1.default.readFileSync(schemaFilenameWithPath, 'utf8');
-        });
-        it('should find content', () => {
-            expect(JSON.parse(schemaContent)).toBeTruthy();
-        });
-        it('should receive an object', () => {
-            let schemaObj = JSON.parse(schemaContent);
-            schemaInstance = new Schema_js_1.default(schemaObj);
-            expect(schemaInstance).toBeTruthy();
-        });
-        it('should get fields in schema', () => {
-            let schemaObj = JSON.parse(schemaContent);
-            schemaInstance = new Schema_js_1.default(schemaObj);
-            expect(schemaInstance.getSchema()).toBeTruthy();
-        });
-        /*
-            
-                it('should count fields', () => {
-                  let schema = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
-                  expect(schemaInstance.getFieldsFromSchema()).toEqual('object');
-                });
-            
-                it('should remove duplicate field by key', () => {
-                  let schemaNormalized = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
-                  fs.writeFileSync('src/clean_application.json', JSON.stringify(schemaNormalized, null, 2));
-                });
-            
-                it('should count number of objects', () => {
-                  let schema = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
-                  expect(Object.entries(schema).length).toEqual(1);
-                });
-                */
+    it('should stringify schema as a string', () => {
+        let str = JSON.stringify(schemaObj);
+        expect(str).toMatch('test');
     });
+    it('should find content', () => {
+        expect(JSON.parse(schemaContent)).toBeTruthy();
+    });
+    it('should get schema', () => {
+        let schemaObj = JSON.parse(schemaContent);
+        schemaInstance = new Schema_js_1.default(schemaObj);
+        expect(schemaInstance.getSchema()).toBeTruthy();
+    });
+    /*
+        
+            it('should count fields', () => {
+              let schema = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
+              expect(schemaInstance.getFieldsFromSchema()).toEqual('object');
+            });
+        
+            it('should remove duplicate field by key', () => {
+              let schemaNormalized = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
+              fs.writeFileSync('src/clean_application.json', JSON.stringify(schemaNormalized, null, 2));
+            });
+        
+            it('should count number of objects', () => {
+              let schema = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
+              expect(Object.entries(schema).length).toEqual(1);
+            });
+            */
 });
--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -1,66 +1,57 @@
 import fs from 'fs';
 import Schema from "../Schema.js";
 
-describe('schema', () => {
+describe('Schema', () => {
   let schemaFilenameWithPath: string;
   let schemaInstance: Schema;
+  let schemaContent: string;
+
+  let schemaObj = {
+    test: "test"
+  }
 
   beforeAll(() => {
     schemaFilenameWithPath = 'mock.json';
+    schemaContent = fs.readFileSync(schemaFilenameWithPath, 'utf8');
   });
 
-  describe('setup', () => {
-    it('should stringify schema as a string', () => {
-      let schemaObj = {
-        test: "test"
-      }
+  it('should receive an object', () => {
+    let schemaObj = JSON.parse(schemaContent);
 
-      let str = JSON.stringify(schemaObj);
-
-      expect(str).toMatch('test');
-    });
+    schemaInstance = new Schema(schemaObj);
+    expect(schemaInstance).toBeTruthy();
   });
 
-  describe('schema', () => {
-    let schemaContent: string;    
-
-    beforeAll(() => {
-      schemaContent = fs.readFileSync(schemaFilenameWithPath, 'utf8');
-    });
-    
-    it('should find content', () => {
-      expect(JSON.parse(schemaContent)).toBeTruthy();
-    });
-
-    it('should receive an object', () => {
-      let schemaObj = JSON.parse(schemaContent);
-
-      schemaInstance = new Schema(schemaObj);
-      expect(schemaInstance).toBeTruthy();
-    });
-
-    it('should get schema', () => {
-      let schemaObj = JSON.parse(schemaContent);
-      schemaInstance = new Schema(schemaObj);
-      expect(schemaInstance.getSchema()).toBeTruthy();
-    });
-
-/*        
-    
-        it('should count fields', () => {
-          let schema = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
-          expect(schemaInstance.getFieldsFromSchema()).toEqual('object');
-        });
-    
-        it('should remove duplicate field by key', () => {
-          let schemaNormalized = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
-          fs.writeFileSync('src/clean_application.json', JSON.stringify(schemaNormalized, null, 2));
-        });
-    
-        it('should count number of objects', () => {
-          let schema = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
-          expect(Object.entries(schema).length).toEqual(1);
-        });
-        */
+  it('should stringify schema as a string', () => {
+    let str = JSON.stringify(schemaObj);
+    expect(str).toMatch('test');
   });
+
+  it('should find content', () => {
+    expect(JSON.parse(schemaContent)).toBeTruthy();
+  });
+
+  it('should get schema', () => {
+    let schemaObj = JSON.parse(schemaContent);
+    schemaInstance = new Schema(schemaObj);
+    expect(schemaInstance.getSchema()).toBeTruthy();
+  });
+
+  /*        
+      
+          it('should count fields', () => {
+            let schema = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
+            expect(schemaInstance.getFieldsFromSchema()).toEqual('object');
+          });
+      
+          it('should remove duplicate field by key', () => {
+            let schemaNormalized = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
+            fs.writeFileSync('src/clean_application.json', JSON.stringify(schemaNormalized, null, 2));
+          });
+      
+          it('should count number of objects', () => {
+            let schema = JSON.parse(fs.readFileSync(schemaFilenameWithPath, 'utf8'));
+            expect(Object.entries(schema).length).toEqual(1);
+          });
+          */
 });
