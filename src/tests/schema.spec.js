850c485ef71ee556ae5f3d0980414cf2c2f52af6
Nate Sire <nate.sire@fairwaymc.com>
2023-03-05 09:16:12 -0500

    test two files

--- src/tests/schema.spec.js
+++ src/tests/schema.spec.js
@@ -10,93 +10,109 @@ describe("Class Schema", () => {
     let schemaFilename;
     let schemaInstance;
     let schemaContentFromFile;
-    beforeAll(() => {
-        schemaFilename = path_1.default.join(__dirname, 'mock.json');
-        schemaContentFromFile = fs_1.default.readFileSync(schemaFilename, "utf8");
+    describe("mock_application.versions.json", () => {
+        beforeAll(() => {
+            schemaFilename = path_1.default.join(__dirname, "mock_application.versions.json");
+            schemaContentFromFile = fs_1.default.readFileSync(schemaFilename, "utf8");
+        });
+        it("should return a schema instance", () => {
+            schemaInstance = new Schema_js_1.default(schemaContentFromFile);
+            expect(schemaInstance).toBeDefined();
+        });
     });
-    it("should instantiate Schema", () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        schemaInstance = new Schema_js_1.default(schemaObj);
-        expect(schemaInstance).toBeTruthy();
-    });
-    it("should parse JSON from file", () => {
-        expect(JSON.parse(schemaContentFromFile)).toBeTruthy();
-    });
-    it("should get schema", () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        schemaInstance = new Schema_js_1.default(schemaObj);
-        expect(schemaInstance.getSchema()).toBeTruthy();
-    });
-    it("should get schema", () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        schemaInstance = new Schema_js_1.default(schemaObj);
-        expect(schemaInstance.getSchema()).toBeTruthy();
-    });
-    it("should get schema item by key", () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        schemaInstance = new Schema_js_1.default(schemaObj);
-        expect(schemaInstance.getSchema('versions')).toBeTruthy();
-    });
-    describe('standard NodeJS behavior', () => {
-        describe('JSON', () => {
-            it("parse should remove duplicate JSON object by key", () => {
-                let schemaObj = JSON.parse(schemaContentFromFile);
-                expect(Object.entries(schemaObj).length).toBe(1);
+    describe("mock.json", () => {
+        beforeAll(() => {
+            schemaFilename = path_1.default.join(__dirname, "mock.json");
+            schemaContentFromFile = fs_1.default.readFileSync(schemaFilename, "utf8");
+        });
+        it("should instantiate Schema", () => {
+            let schemaObj = JSON.parse(schemaContentFromFile);
+            schemaInstance = new Schema_js_1.default(schemaObj);
+            expect(schemaInstance).toBeTruthy();
+        });
+        it("should parse JSON from file", () => {
+            expect(JSON.parse(schemaContentFromFile)).toBeTruthy();
+        });
+        it("should get schema", () => {
+            let schemaObj = JSON.parse(schemaContentFromFile);
+            schemaInstance = new Schema_js_1.default(schemaObj);
+            expect(schemaInstance.getSchema()).toBeTruthy();
+        });
+        it("should get schema", () => {
+            let schemaObj = JSON.parse(schemaContentFromFile);
+            schemaInstance = new Schema_js_1.default(schemaObj);
+            expect(schemaInstance.getSchema()).toBeTruthy();
+        });
+        it("should get schema item by key", () => {
+            let schemaObj = JSON.parse(schemaContentFromFile);
+            schemaInstance = new Schema_js_1.default(schemaObj);
+            expect(schemaInstance.getSchema("versions")).toBeTruthy();
+        });
+        describe("standard NodeJS behavior", () => {
+            describe("JSON", () => {
+                it("parse should remove duplicate JSON object by key", () => {
+                    let schemaObj = JSON.parse(schemaContentFromFile);
+                    expect(Object.entries(schemaObj).length).toBe(1);
+                });
+                it("should return last duplicate JSON object according to IEEE", () => {
+                    let schemaObj = JSON.parse(schemaContentFromFile);
+                    expect(Object.entries(schemaObj["versions"][0]["thirdKey"])).toEqual([
+                        ["test", "test"],
+                    ]);
+                });
             });
-            it("should return last duplicate JSON object according to IEEE", () => {
-                let schemaObj = JSON.parse(schemaContentFromFile);
-                expect(Object.entries(schemaObj['versions'][0]['thirdKey'])).toEqual([["test", "test"]]);
+        });
+        it("should find key for duplicate object", () => {
+            let schemaObj = JSON.parse(schemaContentFromFile);
+            let lastObj = schemaObj["versions"][0]["objects"];
+            expect(lastObj[0]).toEqual({ key: "object_3" });
+        });
+        it("should find duplicate keys inside object", () => {
+            let schemaObj = JSON.parse(schemaContentFromFile);
+            let lastObj = schemaObj["versions"][0]["objects"];
+            expect(Object.entries(lastObj).length).toEqual(2);
+        });
+        it("should find duplicate keys inside object", () => {
+            let schemaObj = JSON.parse(schemaContentFromFile);
+            let lastObj = schemaObj["versions"][0]["objects"];
+            let schema = new Schema_js_1.default(lastObj);
+            expect(schema).toEqual({
+                schemaData: [{ key: "object_3" }, { key: "object_3" }],
             });
         });
-    });
-    it('should find key for duplicate object', () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        let lastObj = schemaObj['versions'][0]['objects'];
-        expect(lastObj[0]).toEqual({ "key": "object_3" });
-    });
-    it('should find duplicate keys inside object', () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        let lastObj = schemaObj['versions'][0]['objects'];
-        expect(Object.entries(lastObj).length).toEqual(2);
-    });
-    it('should find duplicate keys inside object', () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        let lastObj = schemaObj['versions'][0]['objects'];
-        let schema = new Schema_js_1.default(lastObj);
-        expect(schema).toEqual({ "schemaData": [{ "key": "object_3" }, { "key": "object_3" }] });
-    });
-    it('should find duplicate keys inside object', () => {
-        let literal2 = [];
-        let literal = [{ 'key': 1 }, { 'key': 2 }];
-        literal.map(item => {
-            literal2.push(item);
+        it("should find duplicate keys inside object", () => {
+            let literal2 = [];
+            let literal = [{ key: 1 }, { key: 2 }];
+            literal.map((item) => {
+                literal2.push(item);
+            });
+            expect(literal2.length).toEqual(2);
         });
-        expect(literal2.length).toEqual(2);
-    });
-    it('should create unique map', () => {
-        let uniqueObjects = new Map();
-        let literal = [{ 'key': 1 }, { 'key': 1 }];
-        literal.map(item => {
-            uniqueObjects.set(item.key, item);
+        it("should create unique map", () => {
+            let uniqueObjects = new Map();
+            let literal = [{ key: 1 }, { key: 1 }];
+            literal.map((item) => {
+                uniqueObjects.set(item.key, item);
+            });
+            expect(uniqueObjects.size).toEqual(1);
         });
-        expect(uniqueObjects.size).toEqual(1);
-    });
-    it('should create unique map', () => {
-        let uniqueObjects = new Map();
-        let literal = [{ 'key': 1 }, { 'key': 1 }, { 'key': 2 }];
-        literal.map(item => {
-            uniqueObjects.set(item.key, item);
+        it("should create unique map of two objects", () => {
+            let uniqueMapObjects = new Map();
+            let literal = [{ key: 1 }, { key: 1 }, { key: 2 }];
+            literal.map((item) => {
+                uniqueMapObjects.set(item.key, item);
+            });
+            expect(uniqueMapObjects.size).toEqual(2);
         });
-        expect(uniqueObjects.size).toEqual(2);
+        /*
+      it('should find duplicate keys inside object', () => {
+          let schemaObj = JSON.parse(schemaContentFromFile);
+          let lastObj = schemaObj['versions'][0]['objects'];
+          let schema = new Schema(lastObj);
+          schema.removeDuplicates();
+    
+          expect(schema.schemaData).toEqual({"schemaData": [{"key": "object_3"}, {"key": "object_3"}]});
+      });
+    */
     });
-    /*
-    it('should find duplicate keys inside object', () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        let lastObj = schemaObj['versions'][0]['objects'];
-        let schema = new Schema(lastObj);
-        schema.removeDuplicates();
-  
-        expect(schema.schemaData).toEqual({"schemaData": [{"key": "object_3"}, {"key": "object_3"}]});
-    });
-  */
 });
--- src/tests/schema.spec.ts
+++ src/tests/schema.spec.ts
@@ -7,103 +7,119 @@ describe("Class Schema", () => {
   let schemaInstance: Schema;
   let schemaContentFromFile: string;
 
-  beforeAll(() => {
-    schemaFilename = path.join(__dirname, 'mock.json');
-    schemaContentFromFile = fs.readFileSync(schemaFilename, "utf8");
-  });
+  describe("mock_application.versions.json", () => {
+    beforeAll(() => {
+      schemaFilename = path.join(__dirname, "mock_application.versions.json");
+      schemaContentFromFile = fs.readFileSync(schemaFilename, "utf8");
+    });
 
-  it("should instantiate Schema", () => {
-    let schemaObj = JSON.parse(schemaContentFromFile);
-    schemaInstance = new Schema(schemaObj);
-    expect(schemaInstance).toBeTruthy();
-  });
-
-  it("should parse JSON from file", () => {
-    expect(JSON.parse(schemaContentFromFile)).toBeTruthy();
-  });
-
-  it("should get schema", () => {
-    let schemaObj = JSON.parse(schemaContentFromFile);
-    schemaInstance = new Schema(schemaObj);
-    expect(schemaInstance.getSchema()).toBeTruthy();
-  });
-
-  it("should get schema", () => {
-    let schemaObj = JSON.parse(schemaContentFromFile);
-    schemaInstance = new Schema(schemaObj);
-    expect(schemaInstance.getSchema()).toBeTruthy();
-  });
-
-  
-  it("should get schema item by key", () => {
-    let schemaObj = JSON.parse(schemaContentFromFile);
-    schemaInstance = new Schema(schemaObj);
-    expect(schemaInstance.getSchema('versions')).toBeTruthy();
-  });
-
-  describe('standard NodeJS behavior', () => {
-    describe('JSON', () => {
-      it("parse should remove duplicate JSON object by key", () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        expect(Object.entries(schemaObj).length).toBe(1);
-      });
-
-      it("should return last duplicate JSON object according to IEEE", () => {
-        let schemaObj = JSON.parse(schemaContentFromFile);
-        expect(Object.entries(schemaObj['versions'][0]['thirdKey'])).toEqual([[ "test", "test" ]]);
-      });
+    it("should return a schema instance", () => {
+      schemaInstance = new Schema(schemaContentFromFile);
+      expect(schemaInstance).toBeDefined();
     });
   });
 
-  it('should find key for duplicate object', () => {
-      let schemaObj = JSON.parse(schemaContentFromFile);
-      let lastObj = schemaObj['versions'][0]['objects'];
-      expect(lastObj[0]).toEqual({ "key": "object_3" });
-  });
+  describe("mock.json", () => {
+    beforeAll(() => {
+      schemaFilename = path.join(__dirname, "mock.json");
+      schemaContentFromFile = fs.readFileSync(schemaFilename, "utf8");
+    });
 
-  it('should find duplicate keys inside object', () => {
+    it("should instantiate Schema", () => {
       let schemaObj = JSON.parse(schemaContentFromFile);
-      let lastObj = schemaObj['versions'][0]['objects'];
+      schemaInstance = new Schema(schemaObj);
+      expect(schemaInstance).toBeTruthy();
+    });
+
+    it("should parse JSON from file", () => {
+      expect(JSON.parse(schemaContentFromFile)).toBeTruthy();
+    });
+
+    it("should get schema", () => {
+      let schemaObj = JSON.parse(schemaContentFromFile);
+      schemaInstance = new Schema(schemaObj);
+      expect(schemaInstance.getSchema()).toBeTruthy();
+    });
+
+    it("should get schema", () => {
+      let schemaObj = JSON.parse(schemaContentFromFile);
+      schemaInstance = new Schema(schemaObj);
+      expect(schemaInstance.getSchema()).toBeTruthy();
+    });
+
+    it("should get schema item by key", () => {
+      let schemaObj = JSON.parse(schemaContentFromFile);
+      schemaInstance = new Schema(schemaObj);
+      expect(schemaInstance.getSchema("versions")).toBeTruthy();
+    });
+
+    describe("standard NodeJS behavior", () => {
+      describe("JSON", () => {
+        it("parse should remove duplicate JSON object by key", () => {
+          let schemaObj = JSON.parse(schemaContentFromFile);
+          expect(Object.entries(schemaObj).length).toBe(1);
+        });
+
+        it("should return last duplicate JSON object according to IEEE", () => {
+          let schemaObj = JSON.parse(schemaContentFromFile);
+          expect(Object.entries(schemaObj["versions"][0]["thirdKey"])).toEqual([
+            ["test", "test"],
+          ]);
+        });
+      });
+    });
+
+    it("should find key for duplicate object", () => {
+      let schemaObj = JSON.parse(schemaContentFromFile);
+      let lastObj = schemaObj["versions"][0]["objects"];
+      expect(lastObj[0]).toEqual({ key: "object_3" });
+    });
+
+    it("should find duplicate keys inside object", () => {
+      let schemaObj = JSON.parse(schemaContentFromFile);
+      let lastObj = schemaObj["versions"][0]["objects"];
       expect(Object.entries(lastObj).length).toEqual(2);
-  });
+    });
 
-  it('should find duplicate keys inside object', () => {
+    it("should find duplicate keys inside object", () => {
       let schemaObj = JSON.parse(schemaContentFromFile);
-      let lastObj = schemaObj['versions'][0]['objects'];
+      let lastObj = schemaObj["versions"][0]["objects"];
       let schema = new Schema(lastObj);
 
-      expect(schema).toEqual({"schemaData": [{"key": "object_3"}, {"key": "object_3"}]});
-  });
-
-  it('should find duplicate keys inside object', () => {
-    let literal2 : Array<any> = [];
-    let literal = [ { 'key': 1 }, { 'key': 2} ];
-    literal.map(item => {
-      literal2.push(item);
+      expect(schema).toEqual({
+        schemaData: [{ key: "object_3" }, { key: "object_3" }],
+      });
     });
-    expect(literal2.length).toEqual(2);
-  });
 
-  it('should create unique map', () => {
-    let uniqueObjects = new Map();
-    let literal = [ { 'key': 1 }, { 'key': 1 } ];
-    literal.map(item => {
-      uniqueObjects.set(item.key, item);
+    it("should find duplicate keys inside object", () => {
+      let literal2: Array<any> = [];
+      let literal = [{ key: 1 }, { key: 2 }];
+      literal.map((item) => {
+        literal2.push(item);
+      });
+      expect(literal2.length).toEqual(2);
     });
-    expect(uniqueObjects.size).toEqual(1);
-  });
 
-  it('should create unique map of two items', () => {
-    let uniqueObjects = new Map();
-    let literal = [ { 'key': 1 }, { 'key': 1 }, { 'key': 2 } ];
-
-    literal.map(item => {
-      uniqueObjects.set(item.key, item);
+    it("should create unique map", () => {
+      let uniqueObjects = new Map();
+      let literal = [{ key: 1 }, { key: 1 }];
+      literal.map((item) => {
+        uniqueObjects.set(item.key, item);
+      });
+      expect(uniqueObjects.size).toEqual(1);
     });
-    expect(uniqueObjects.size).toEqual(2);
-  });
 
-  /*
+    it("should create unique map of two objects", () => {
+      let uniqueMapObjects = new Map();
+      let literal = [{ key: 1 }, { key: 1 }, { key: 2 }];
+
+      literal.map((item) => {
+        uniqueMapObjects.set(item.key, item);
+      });
+      expect(uniqueMapObjects.size).toEqual(2);
+    });
+
+    /*
   it('should find duplicate keys inside object', () => {
       let schemaObj = JSON.parse(schemaContentFromFile);
       let lastObj = schemaObj['versions'][0]['objects'];
@@ -113,5 +129,5 @@ describe("Class Schema", () => {
       expect(schema.schemaData).toEqual({"schemaData": [{"key": "object_3"}, {"key": "object_3"}]});
   });
 */
-
+  });
 });
